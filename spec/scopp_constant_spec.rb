92e3e923b0651c8503ec5dcf4807485b5ff1355a
Sou <ghouno05@aim.com>
2017-04-29 12:49:26 +0000

    stop spec pushbullet

--- spec/scopp_constant_spec.rb
+++ spec/scopp_constant_spec.rb
@@ -6,13 +6,13 @@ RSpec.describe ScoppConstant, :type => :model do
     expect(ScoppConstant.get_url(500)).to eq("http://api.syosetu.com/novelapi/api/?out=json&lim=500&start=500&gzip=5")
   end
   
-  it "push note"do
-    title="title title"
-    body="body asiegarga"
-    push=ScoppConstant.pushbullet_note(title,body)
-    expect(push.body["active"]).to eq(true)
-    expect(push.body["type"]).to eq("note")
-    expect(push.body["title"]).to eq(title)
-    expect(push.body["body"]).to eq(body)
-  end
+  # it "push note"do
+  #   title="title title"
+  #   body="body asiegarga"
+  #   push=ScoppConstant.pushbullet_note(title,body)
+  #   expect(push.body["active"]).to eq(true)
+  #   expect(push.body["type"]).to eq("note")
+  #   expect(push.body["title"]).to eq(title)
+  #   expect(push.body["body"]).to eq(body)
+  # end
 end
