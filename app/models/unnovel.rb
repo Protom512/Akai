0121ab52aa8ad9079dbab710791d6d295d3dd41d
Protom512 <ghouno05@gmail.com>
2017-06-15 11:44:13 +0900

    Applied fixes from SideCI (#28)

--- app/models/unnovel.rb
+++ app/models/unnovel.rb
@@ -1,54 +1,53 @@
 require 'zlib'
 class Unnovel < ApplicationRecord
-    # has_one :novel
-    STATUS=["daily","weekly","monthly","yearly"]
-    def intialize(date=Date.today,duration =1.week)
-        @data=data
-    end   
-    def self.get_data
-        bot = Discordrb::Bot.new token: ENV['DISCORD_TOKEN'], client_id: ENV['DISCORD_CLIENT_ID']
-        bot.run :async
-        before=Update.count
-        jsons=Array.new
-        urls=Array.new
-        urls.push(ScoppConstant.get_url(1))
-        Parallel.each(urls,in_threads: 4){|url|
-            uri = URI.parse(url)
-            gzip = Net::HTTP.get(uri)
-            jsons.push(ActiveSupport::Gzip.decompress(gzip))
-        }
-        jsons.each{|json|
-        js = ActiveSupport::JSON.decode(json)
-            js.each do |data|
-                if data['allcount'].present?
-                    next
-                end
-                User.set_data(data)
-                Novel.set_data(data)
-                Update.set_data(data)
-            end
-        }
-        after=Update.count
-        text="update done!.\n #{after-before} update record(s) has been added!"
-        bot.send_message(ENV['DISCORD_CHANNEL_ID'], text)
-        bot.stop
-        
+  # has_one :novel
+  STATUS = %w[daily weekly monthly yearly].freeze
+  def intialize(_date = Date.today, _duration = 1.week)
+    @data = data
+  end
+
+  def self.get_data
+    bot = Discordrb::Bot.new token: ENV['DISCORD_TOKEN'], client_id: ENV['DISCORD_CLIENT_ID']
+    bot.run :async
+    before = Update.count
+    jsons = []
+    urls = []
+    urls.push(ScoppConstant.get_url(1))
+    Parallel.each(urls, in_threads: 4) do |url|
+      uri = URI.parse(url)
+      gzip = Net::HTTP.get(uri)
+      jsons.push(ActiveSupport::Gzip.decompress(gzip))
     end
-    def self.calculate_point
-        novels=Novel.joins(:updates).where(updates: {novel_updated_at:  Date.today...Date.today+1.day})   
-        #daily
-        novels.each{|novel|
-            unnovel_0=Unnovel.new
-            unnovel_1=Unnovel.new
-            unnovel_2=Unnovel.new
-            unnovel_3=Unnovel.new
-            unnovel_0.calc_(Date.today,STATUS[0],novel)
-            unnovel_1.calc_(Date.today,STATUS[1],novel)
-            unnovel_2.calc_(Date.today,STATUS[2],novel)
-            unnovel_3.calc_(Date.today,STATUS[3],novel)
-        }
-        #weekly
-        #monthly
-        #yearly
+    jsons.each do |json|
+      js = ActiveSupport::JSON.decode(json)
+      js.each do |data|
+        next if data['allcount'].present?
+        User.set_data(data)
+        Novel.set_data(data)
+        Update.set_data(data)
+      end
     end
+    after = Update.count
+    text = "update done!.\n #{after - before} update record(s) has been added!"
+    bot.send_message(ENV['DISCORD_CHANNEL_ID'], text)
+    bot.stop
+  end
+
+  def self.calculate_point
+    novels = Novel.joins(:updates).where(updates: { novel_updated_at: Date.today...Date.today + 1.day })
+    # daily
+    novels.each do |novel|
+      unnovel_0 = Unnovel.new
+      unnovel_1 = Unnovel.new
+      unnovel_2 = Unnovel.new
+      unnovel_3 = Unnovel.new
+      unnovel_0.calc_(Date.today, STATUS[0], novel)
+      unnovel_1.calc_(Date.today, STATUS[1], novel)
+      unnovel_2.calc_(Date.today, STATUS[2], novel)
+      unnovel_3.calc_(Date.today, STATUS[3], novel)
+    end
+    # weekly
+    # monthly
+    # yearly
+  end
 end
